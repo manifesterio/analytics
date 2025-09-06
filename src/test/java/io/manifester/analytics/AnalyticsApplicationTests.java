52a952419819a20eecd80b2fc4718e04bd366aaa
arunlakshman <arunmuthuravi@gmail.com>
2025-09-06 01:19:19 -0700

    chore: manifester analytics first commit

---
+++ src/test/java/io/manifester/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.manifester.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
